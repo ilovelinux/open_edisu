efe311e47a86363ff413fc61fee57335b43bcb48
ilovelinux <ilovelinux@users.noreply.github.com>
2022-05-02 00:22:51 +0200

    Remove unused import

--- lib/models/edisu.dart
+++ lib/models/edisu.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:open_edisu/utilities/extensions/date.dart';
 
 import '../utilities/constants/api.dart';
 import '../utilities/extensions/time.dart';
