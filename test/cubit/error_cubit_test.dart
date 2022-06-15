e21be9b22b46e49f4add614a32cd7ad56175e225
ilovelinux <ilovelinux@users.noreply.github.com>
2022-06-15 16:37:20 +0200

    Fix test

--- test/cubit/error_cubit_test.dart
+++ test/cubit/error_cubit_test.dart
@@ -5,24 +5,24 @@ import 'package:open_edisu/cubit/error_cubit.dart';
 void main() {
   group('ErrorCubit', () {
     blocTest<ErrorCubit, ErrorState>(
-      'emits [] when nothing is called and initial status is InitialState',
+      'Check initial state is InitialState',
       build: () => ErrorCubit(),
       expect: () => const <ErrorState>[],
-      verify: (cubit) => expect(cubit.state, const ErrorState()),
+      verify: (cubit) => expect(cubit.state, const ErrorState.initial()),
     );
 
     blocTest<ErrorCubit, ErrorState>(
-      'emits [dialogError(err)] when showInDialog(message) is called',
+      'emits dialogError(err) when showInDialog(message) is called',
       build: () => ErrorCubit(),
       act: (cubit) => cubit.showInDialog("Error!"),
-      expect: () => const <ErrorState>[ErrorState.dialogError("Error!")],
+      expect: () => const [ErrorState.dialogError("Error!")],
     );
 
     blocTest<ErrorCubit, ErrorState>(
-      'emits [snackBarError(message)] when showInStackBar(message) is called',
+      'emits snackBarError(message) when showInStackBar(message) is called',
       build: () => ErrorCubit(),
       act: (cubit) => cubit.showInSnackBar("Error!"),
-      expect: () => const <ErrorState>[ErrorState.snackBarError("Error!")],
+      expect: () => const [ErrorState.snackBarError("Error!")],
     );
   });
 }
