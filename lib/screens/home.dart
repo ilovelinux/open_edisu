c3e0b54263fb89d401c298868a171ad1dc0c1b13
ilovelinux <ilovelinux@users.noreply.github.com>
2022-06-17 22:08:00 +0200

    Update settings

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:intl/intl.dart';
+import 'package:open_edisu/screens/settings/account.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
--- lib/screens/home_tabs/settings.dart
+++ lib/screens/home_tabs/settings.dart
@@ -1,36 +1,77 @@
 part of '../home.dart';
 
-class _SettingsView extends StatelessWidget {
+class _SettingsView extends StatefulWidget {
   const _SettingsView({Key? key}) : super(key: key);
 
   @override
+  State<_SettingsView> createState() => _SettingsViewState();
+}
+
+class _SettingsViewState extends State<_SettingsView> {
+  late final User user;
+
+  @override
+  void initState() {
+    user = context
+        .read<AuthBloc>()
+        .state
+        .whenOrNull(authenticated: (user) => user)!;
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    // final kDebugMode = false;
+
     return ListView(
       children: [
         ListTile(
-          title: const Text("About"),
-          onTap: () => showDialog(
-            context: context,
-            // TODO: Use AboutDialog()
-            builder: (_) => _AboutDialog(),
+          title: Text(
+            "User",
+            style: TextStyle(color: Theme.of(context).primaryColor),
           ),
         ),
         ListTile(
+          leading: const Icon(Icons.person),
+          title: const Text("Account"),
+          trailing: const Icon(Icons.arrow_forward_ios),
+          onTap: () => Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => AccountView(),
+            ),
+          ),
+        ),
+        ListTile(
+          leading: const Icon(Icons.notifications),
+          title: const Text("Notifications"),
+          trailing: const Icon(Icons.arrow_forward_ios),
+        ),
+        const Divider(),
+        ListTile(
+          leading: const Icon(Icons.exit_to_app),
           title: const Text("Logout"),
           onTap: () => context.read<AuthBloc>().add(const AuthEvent.logout()),
+        ),
+        const Divider(),
+        ListTile(
+          title: Text(
+            "Other",
+            style: TextStyle(
+              color: Theme.of(context).primaryColor,
+            ),
+          ),
+        ),
+        AboutListTile(
+          applicationIcon: Image.asset(
+            'assets/icon/icon.png',
+            scale: 40,
+          ),
+          applicationName: "Open Edisu",
+          applicationVersion: "0.1.2",
+          applicationLegalese: "Released under GPLv3",
+          icon: const Icon(Icons.info),
+          child: const Text("About"),
         )
-      ],
-    );
-  }
-}
-
-class _AboutDialog extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return const SimpleDialog(
-      title: Text("About"),
-      children: [
-        Text("App svilu"),
       ],
     );
   }
