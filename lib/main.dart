8a111ccaa0652b858184467d42eac324d6142a83
Antonio Spadaro <ilovelinux@users.noreply.github.com>
2024-03-26 10:08:48 +0100

    Add Desktop view

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'dart:io';
+
+import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:open_edisu/features/home/ui/screens/home_desktop.dart';
 
 import 'core/utilities/errors.dart';
 import 'core/utilities/inceptor.dart';
@@ -18,13 +22,15 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initInceptor();
 
-  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
-  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-    statusBarBrightness: Brightness.light,
-    statusBarIconBrightness: Brightness.dark,
-    statusBarColor: Colors.transparent,
-    systemNavigationBarColor: Colors.transparent,
-  ));
+  if (Platform.isAndroid) {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarBrightness: Brightness.light,
+      statusBarIconBrightness: Brightness.dark,
+      statusBarColor: Colors.transparent,
+      systemNavigationBarColor: Colors.transparent,
+    ));
+  }
 
   runApp(const MyApp());
 }
@@ -43,52 +49,110 @@ class MyApp extends StatelessWidget {
           create: (_) => BookingsBloc(),
         ),
       ],
-      child: MaterialApp(
-        title: 'Open Edisu',
-        localizationsDelegates: AppLocalizations.localizationsDelegates,
-        supportedLocales: AppLocalizations.supportedLocales,
-        theme: ThemeData(
-          colorSchemeSeed: const Color.fromRGBO(0xd0, 0x36, 0x29, 1),
-        ),
-        darkTheme: ThemeData(
-          colorSchemeSeed: const Color.fromRGBO(0xd0, 0x36, 0x29, 1),
-          brightness: Brightness.dark,
-        ),
-        routes: {
-          'login': (_) => const LoginPage(),
-          'signup': (_) => const SignupPage(),
-        },
-        home: BlocConsumer<AuthBloc, AuthState>(
-          listener: (context, state) => state.whenOrNull<void>(
-            authenticated: (user) {
-              GetIt.I.registerSingleton<User>(user);
-              context.read<BookingsBloc>().add(const BookingsEvent.update());
-            },
-            unauthenticated: (
-              final sessionExpired,
-              final connectionError,
-              final message,
-            ) {
-              if (GetIt.I.isRegistered<User>()) {
-                GetIt.I.unregister<User>();
-              }
+      child: Platform.isAndroid ? const MobileApp() : const DesktopApp(),
+    );
+  }
+}
 
-              final error = connectionError
-                  ? AppLocalizations.of(context)!.connectionError
-                  : sessionExpired
-                      ? AppLocalizations.of(context)!.sessionExpired
-                      : message;
+class DesktopApp extends StatelessWidget {
+  const DesktopApp({super.key});
 
-              if (error != null) {
-                showErrorInSnackbar(context, error);
-              }
-            },
-          ),
-          builder: (context, state) => state.when(
-            authenticated: (_) => const HomePage(),
-            unauthenticated: (_, __, ___) => const LoginPage(),
-            unknown: () => const Scaffold(body: LoadingWidget()),
-          ),
+  @override
+  Widget build(BuildContext context) {
+    return FluentApp(
+      title: 'Open Edisu',
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      routes: {
+        'login': (_) => const LoginPage(),
+        'signup': (_) => const SignupPage(),
+      },
+      home: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state) => state.whenOrNull<void>(
+          authenticated: (user) {
+            GetIt.I.registerSingleton<User>(user);
+            context.read<BookingsBloc>().add(const BookingsEvent.update());
+          },
+          unauthenticated: (
+            final sessionExpired,
+            final connectionError,
+            final message,
+          ) {
+            if (GetIt.I.isRegistered<User>()) {
+              GetIt.I.unregister<User>();
+            }
+
+            final error = connectionError
+                ? AppLocalizations.of(context)!.connectionError
+                : sessionExpired
+                    ? AppLocalizations.of(context)!.sessionExpired
+                    : message;
+
+            if (error != null) {
+              showErrorInSnackbar(context, error);
+            }
+          },
+        ),
+        builder: (context, state) => state.when(
+          authenticated: (_) => const HomePageDesktop(),
+          unauthenticated: (_, __, ___) => const LoginPage(),
+          unknown: () => const Scaffold(body: LoadingWidget()),
+        ),
+      ),
+    );
+  }
+}
+
+class MobileApp extends StatelessWidget {
+  const MobileApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Open Edisu',
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      theme: ThemeData(
+        colorSchemeSeed: const Color.fromRGBO(0xd0, 0x36, 0x29, 1),
+      ),
+      darkTheme: ThemeData(
+        colorSchemeSeed: const Color.fromRGBO(0xd0, 0x36, 0x29, 1),
+        brightness: Brightness.dark,
+      ),
+      routes: {
+        'login': (_) => const LoginPage(),
+        'signup': (_) => const SignupPage(),
+      },
+      home: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state) => state.whenOrNull<void>(
+          authenticated: (user) {
+            GetIt.I.registerSingleton<User>(user);
+            context.read<BookingsBloc>().add(const BookingsEvent.update());
+          },
+          unauthenticated: (
+            final sessionExpired,
+            final connectionError,
+            final message,
+          ) {
+            if (GetIt.I.isRegistered<User>()) {
+              GetIt.I.unregister<User>();
+            }
+
+            final error = connectionError
+                ? AppLocalizations.of(context)!.connectionError
+                : sessionExpired
+                    ? AppLocalizations.of(context)!.sessionExpired
+                    : message;
+
+            if (error != null) {
+              showErrorInSnackbar(context, error);
+            }
+          },
+        ),
+        builder: (context, state) => state.when(
+          authenticated: (_) => const HomePage(),
+          unauthenticated: (_, __, ___) => const LoginPage(),
+          unknown: () => const Scaffold(body: LoadingWidget()),
         ),
       ),
     );
--- linux/flutter/generated_plugins.cmake
+++ linux/flutter/generated_plugins.cmake
@@ -5,7 +5,9 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   flutter_secure_storage_linux
   maps_launcher
+  screen_retriever
   url_launcher_linux
+  window_manager
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
